feaf40280c41fe3889e3ba720ad75522bf22d5cc
Verena Blaschke <verena.blaschke@gmx.de>
2019-07-08 15:51:52 +0200

    Improve multi-word search/highlighting

---
+++ src/main/java/de/ws1819/colewe/client/ClickPanel.java
@@ -0,0 +1,29 @@
+package de.ws1819.colewe.client;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.event.dom.client.HasMouseDownHandlers;
+import com.google.gwt.event.dom.client.MouseDownEvent;
+import com.google.gwt.event.dom.client.MouseDownHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.safehtml.shared.SafeHtml;
+import com.google.gwt.user.client.ui.HTMLPanel;
+
+public class ClickPanel extends HTMLPanel implements HasClickHandlers, HasMouseDownHandlers {
+
+	public ClickPanel(SafeHtml safeHtml) {
+		super(safeHtml);
+	}
+
+	@Override
+	public HandlerRegistration addClickHandler(ClickHandler handler) {
+		return addDomHandler(handler, ClickEvent.getType());
+	}
+
+	@Override
+	public HandlerRegistration addMouseDownHandler(MouseDownHandler handler) {
+		return addDomHandler(handler, MouseDownEvent.getType());
+	}
+
+}
