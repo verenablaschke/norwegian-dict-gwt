5f546d08562fd500c96557d968c409fcd28d100a
Verena Blaschke <verena.blaschke@gmx.de>
2019-08-27 13:52:06 +0200

    Recursive compound splitting #1

--- src/main/java/de/ws1819/colewe/server/DictionaryServiceImpl.java
+++ src/main/java/de/ws1819/colewe/server/DictionaryServiceImpl.java
@@ -23,25 +23,10 @@ public class DictionaryServiceImpl extends RemoteServiceServlet implements Dicti
 	public ArrayList<Entry> query(String word) throws IllegalArgumentException {
 		logger.info("QUERY: " + word);
 		ListMultimap<String, Entry> entries = (ListMultimap<String, Entry>) getServletContext().getAttribute("entries");
-		ArrayList<Entry> results = querySingleWord(entries, word);
-		if (results.isEmpty()) {
-			// Attempt compound splitting.
-			String first, second;
-			for (int i = 1; i < word.length() - 1; i++) {
-				first = word.substring(0, i);
-				ArrayList<Entry> resultsFirst = querySingleWord(entries, first);
-				if (resultsFirst.isEmpty()) {
-					continue;
-				}
-				second = word.substring(i);
-				ArrayList<Entry> resultsSecond = querySingleWord(entries, second);
-				if (resultsSecond.isEmpty()) {
-					continue;
-				}
-				results.addAll(resultsFirst);
-				results.addAll(resultsSecond);
-				break;
-			}
+		ArrayList<Entry> results = queryWithPossibleSplit(entries, word);
+		logger.info("RESULTS: " + word);
+		for (Entry entry : results) {
+			logger.info("-- " + entry.toString());
 		}
 		return results;
 	}
@@ -55,42 +40,45 @@ public class DictionaryServiceImpl extends RemoteServiceServlet implements Dicti
 		return new ArrayList<Entry>(results);
 	}
 
-//	private ArrayList<Entry> query(ListMultimap<String, Entry> entries, String word) {
-//		ArrayList<Entry> results = querySingleWord(entries, word);
-//		if (results.isEmpty()) {
-//			// Attempt compound splitting.
-//			String first, second;
-//			for (int i = 1; i < word.length() - 1; i++) {
-//				first = word.substring(0, i);
-//				ArrayList<Entry> resultsFirst = querySingleWord(entries, first);
-//				if (resultsFirst.isEmpty()) {
-//					continue;
-//				}
-//				second = word.substring(i);
-//				ArrayList<Entry> resultsSecond = querySingleWord(entries, second);
-//				if (resultsSecond.isEmpty()) {
-//					if (i < word.length() - 3 && (second.startsWith("e") || second.startsWith("s"))) {
-//						// -e- and -s- can be used to join compounds
-//						resultsSecond = querySingleWord(entries, second.substring(1));
-//						if (!resultsSecond.isEmpty()) {
-//							results.addAll(resultsFirst);
-//							results.addAll(resultsSecond);
-//							break;
-//						}
-//					}
-//					
-//					
-//					resultsSecond = query(entries, second);
-//					// TODO -e-, -s-
-//					if (! resultsSecond.isEmpty()){
-//						break;
-//					}
-//				}
-//				results.addAll(resultsFirst);
-//				results.addAll(resultsSecond);
-//				break;
-//			}
-//		}
-//		return results;
-//	}
+	private ArrayList<Entry> queryWithPossibleSplit(ListMultimap<String, Entry> entries, String word) {
+		ArrayList<Entry> results = querySingleWord(entries, word);
+		if (results.isEmpty()) {
+			// Attempt compound splitting.
+			String first, second;
+			// Try to do the least amount of splits possible.
+			for (int i = 1; i < word.length() - 1; i++) {
+				first = word.substring(0, i);
+				ArrayList<Entry> resultsFirst = querySingleWord(entries, first);
+				if (resultsFirst.isEmpty()) {
+					continue;
+				}
+				second = word.substring(i);
+				ArrayList<Entry> resultsSecond = querySingleWord(entries, second);
+				if (resultsSecond.isEmpty()) {
+					continue;
+				}
+				results.addAll(resultsFirst);
+				results.addAll(resultsSecond);
+				return results;
+			}
+			
+			// Attempt more splits.
+			for (int i = 1; i < word.length() - 1; i++) {
+				first = word.substring(0, i);
+				ArrayList<Entry> resultsFirst = querySingleWord(entries, first);
+				if (resultsFirst.isEmpty()) {
+					continue;
+				}
+				second = word.substring(i);
+				ArrayList<Entry> resultsSecond = queryWithPossibleSplit(entries, second);
+				if (resultsSecond.isEmpty()) {
+					continue;
+				}
+				results.addAll(resultsFirst);
+				results.addAll(resultsSecond);
+				break;
+			}
+		}
+		return results;
+	}
 }
